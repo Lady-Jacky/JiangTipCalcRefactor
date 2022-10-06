a1d0b8c6c2db8c8bd02dc7123c51d9128f702617
1E10 Laptop <sample@bths.edu>
2022-10-06 11:01:36 -0400

    Stuff

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator {
+    private int numPeople;
+    private int percentage;
+    private double totalBillBeforeTip = 0.0;
+
+    public TipCalculator(int numPeople, int percentage){
+        this.numPeople = numPeople;
+        this.percentage = percentage;
+    }
+
+    public int getTipPercentage() {
+        return percentage;
+    }
+
+    public void addMeal(double cost){
+        totalBillBeforeTip += cost;
+    }
+
+
 }
